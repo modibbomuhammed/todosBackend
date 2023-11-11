42552e13256f3c06a41da373b6f7c790328771a4
Muhammed Modibbo <modibbomuhammed@gmail.com>
2023-11-11 18:59:58 +0000

    body parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express"),
     app = express(),
     mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
     port = 3000;
 
 
@@ -9,9 +10,31 @@ mongoose.connect('mongodb://127.0.0.1:27017/test')
     .catch(err => console.log({ err }));
 
 
-app.get('/', (req, res) => {
-    console.log({ app })
-    res.json({ greeting: 'Hello World!!' })
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// model
+const { Schema, model } = mongoose;
+
+
+const TodoSchema = new Schema({
+    title: String,
+    completed: Boolean,
+    date: Date
+});
+
+const Todo = model('todo', TodoSchema);
+
+app.get('/', async (req, res) => {
+    const todos = await Todo.find({})
+    console.log({ todos });
+    res.json({ greeting: 'Hello World!!', todos })
+});
+
+app.post('/todo', async (req, res) => {
+    const { message } = req.body;
+    const newTodo = await Todo.create({ title: message, completed: false });
+    console.log({ newTodo });
+    res.json({ message: true });
 });
 
 app.listen(port, () => {
